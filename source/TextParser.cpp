4ed8e44541da879a9023b7b2c9e4d3832bea4f4c
neveroyatniy-bobr <nicant2007@ygmail.com>
2025-09-16 23:17:01 +0300

    Использовал MyVector в TextParser

--- source/TextParser.cpp
+++ source/TextParser.cpp
@@ -5,6 +5,7 @@
 #include <assert.h>
 
 #include "MyStringFunctions.h"
+#include "MyVector.h"
 
 size_t TextParse(char*** text_ptr, const char* input_file_name) {
     assert(text_ptr != NULL);
@@ -17,32 +18,26 @@ size_t TextParse(char*** text_ptr, const char* input_file_name) {
         return 0;
     }
 
-    size_t n = 16;
-    *text_ptr = (char**)calloc(n, sizeof(*text_ptr[0]));
+    MyVector text_vec = {};
+    MyVectorInit(&text_vec, 16);
 
     size_t m;
     char* line = NULL;
 
-    size_t cnt = 0;
     while (MyGetLine(&line, &m, input_file) != 0 && line != NULL) {
         if (*line != '\0') {
-            (*text_ptr)[cnt] = line;
-            cnt++;
-
+            MyVectorAdd(&text_vec, line);
             line = NULL;
         }
-
-        if (cnt == n) {
-            n *= 2;
-            *text_ptr = (char**)realloc(*text_ptr, n * sizeof(*text_ptr[0]));
-        }
     }
+
+    *text_ptr = text_vec.data;
 
     free(line);
 
     fclose(input_file);
 
-    return cnt;
+    return text_vec.size;
 }
 
 void MemoryFree(char** text, size_t len) {
