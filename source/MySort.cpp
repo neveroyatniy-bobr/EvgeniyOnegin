e02d9e79f8e2884ff2a138770fbd11c9ece4e9e2
neveroyatniy-bobr <nicant2007@ygmail.com>
2025-09-16 20:13:43 +0300

    Исправил if-return-else в MySort()

--- source/MySort.cpp
+++ source/MySort.cpp
@@ -20,52 +20,51 @@ void MyQSort(void* array, size_t len, size_t size, Comparator comp) {
     if (len == 1) {
         return;
     }
-    else {
-        size_t first_half_len = len / 2;
-        size_t second_half_len = len - first_half_len;
+    
+    size_t first_half_len = len / 2;
+    size_t second_half_len = len - first_half_len;
 
-        void* first_half = array;
-        void* second_half = VoidPtrMove(array, first_half_len, size);
+    void* first_half = array;
+    void* second_half = VoidPtrMove(array, first_half_len, size);
 
-        void* first_half_end = VoidPtrMove(first_half, first_half_len, size);
-        void* second_half_end = VoidPtrMove(second_half, second_half_len, size);
+    void* first_half_end = VoidPtrMove(first_half, first_half_len, size);
+    void* second_half_end = VoidPtrMove(second_half, second_half_len, size);
 
-        MyQSort(first_half, first_half_len, size, comp);
-        MyQSort(second_half, second_half_len, size, comp);
+    MyQSort(first_half, first_half_len, size, comp);
+    MyQSort(second_half, second_half_len, size, comp);
 
-        void* merge_array = calloc(len, size);
-        void* const start_merge_array = merge_array;
+    void* merge_array = calloc(len, size);
+    void* const start_merge_array = merge_array;
 
-        while (first_half != first_half_end || second_half != second_half_end) {
-            if (first_half == first_half_end) {
-                memcpy(merge_array, second_half, size);
-                merge_array =  VoidPtrMove(merge_array, 1, size);
-                second_half = VoidPtrMove(second_half, 1, size);
-            }
-            else if (second_half == second_half_end) {
+    while (first_half != first_half_end || second_half != second_half_end) {
+        if (first_half == first_half_end) {
+            memcpy(merge_array, second_half, size);
+            merge_array =  VoidPtrMove(merge_array, 1, size);
+            second_half = VoidPtrMove(second_half, 1, size);
+        }
+        else if (second_half == second_half_end) {
+            memcpy(merge_array, first_half, size);
+            merge_array =  VoidPtrMove(merge_array, 1, size);
+            first_half = VoidPtrMove(first_half, 1, size);
+        }
+        else {
+            if (comp(first_half, second_half) <= 0) {
                 memcpy(merge_array, first_half, size);
                 merge_array =  VoidPtrMove(merge_array, 1, size);
                 first_half = VoidPtrMove(first_half, 1, size);
             }
             else {
-                if (comp(first_half, second_half) <= 0) {
-                    memcpy(merge_array, first_half, size);
-                    merge_array =  VoidPtrMove(merge_array, 1, size);
-                    first_half = VoidPtrMove(first_half, 1, size);
-                }
-                else {
-                    memcpy(merge_array, second_half, size);
-                    merge_array =  VoidPtrMove(merge_array, 1, size);
-                    second_half = VoidPtrMove(second_half, 1, size);
-                }
+                memcpy(merge_array, second_half, size);
+                merge_array =  VoidPtrMove(merge_array, 1, size);
+                second_half = VoidPtrMove(second_half, 1, size);
             }
         }
-
-        memcpy(array, start_merge_array, len * size);
-
-        
-        free(start_merge_array);
     }
+
+    memcpy(array, start_merge_array, len * size);
+
+    
+    free(start_merge_array);
 }
 
 int int_comp(const void* val1, const void* val2) {
