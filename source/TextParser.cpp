95d668e55cae71805e3ce08e9921cd494c721747
neveroyatniy-bobr <nicant2007@ygmail.com>
2025-09-17 19:29:12 +0300

    Реализовал способ с вектором без эвристики

--- source/TextParser.cpp
+++ source/TextParser.cpp
@@ -32,104 +32,21 @@ size_t TextParse(char*** text_ptr, const char* input_file_name) {
     text_buffer[true_file_size] = '\0';
     
     fclose(input_file);
-    
-    const size_t iterations_cnt = 10000;
-    const size_t iterations_to_cache_warm_up_cnt = 100000;
-    
-    clock_t start_time = 0;
-    clock_t end_time = 0;
-
-    printf("Вектор без эвристики: ");
-
-    for (size_t iteration = 0; iteration < iterations_to_cache_warm_up_cnt + iterations_cnt; iteration++) {
-        if (iteration == iterations_to_cache_warm_up_cnt) {
-            start_time = clock();
-        }
-
         
-        MyVector text_vec = {};
-        MyVectorInit(&text_vec, 16);
-    
-        MyVectorAdd(&text_vec, text_buffer);
-        for (size_t i = 0; i < true_file_size; i++) {
-            if (text_buffer[i] == '\n') {
-                text_buffer[i] = '\0';
-                MyVectorAdd(&text_vec, text_buffer + (i + 1));
-            }
+    MyVector text_vec = {};
+    MyVectorInit(&text_vec, 16);
+
+    MyVectorAdd(&text_vec, text_buffer);
+    for (size_t i = 0; i < true_file_size; i++) {
+        if (text_buffer[i] == '\n') {
+            text_buffer[i] = '\0';
+            MyVectorAdd(&text_vec, text_buffer + (i + 1));
         }
-
-        *text_ptr = text_vec.data;
-
-        MyVectorFree(&text_vec);
     }
 
-    end_time = clock();
+    *text_ptr = text_vec.data;
 
-    printf("%lf\n", (double)(end_time - start_time) / (double)iterations_cnt);
-
-    printf("Вектор c эвристикой: ");
-
-    for (size_t iteration = 0; iteration < iterations_to_cache_warm_up_cnt + iterations_cnt; iteration++) {
-        if (iteration == iterations_to_cache_warm_up_cnt) {
-            start_time = clock();
-        }
-
-        size_t appro_line_size = 32;
-        size_t appro_text_size = true_file_size / appro_line_size;
-
-        MyVector text_vec = {};
-        MyVectorInit(&text_vec, appro_text_size);
-
-        MyVectorAdd(&text_vec, text_buffer);
-        for (size_t i = 0; i < true_file_size; i++) {
-            if (text_buffer[i] == '\n') {
-                text_buffer[i] = '\0';
-                MyVectorAdd(&text_vec, text_buffer + (i + 1));
-            }
-        }
-        *text_ptr = text_vec.data;
-
-        MyVectorFree(&text_vec);
-    }
-
-    end_time = clock();
-
-    printf("%lf\n", (double)(end_time - start_time) / (double)iterations_cnt);
-
-    printf("Подсчет строк и одно выделение памяти: ");
-
-    for (size_t iteration = 0; iteration < iterations_to_cache_warm_up_cnt + iterations_cnt; iteration++) {
-        if (iteration == iterations_to_cache_warm_up_cnt) {
-            start_time = clock();
-        }
-
-        size_t line_cnt = 0;
-        for (size_t i = 0; i < true_file_size; i++) {
-            if (text_buffer[i] == '\0') {
-                text_buffer[i] = '\0';
-                line_cnt++;
-            }
-        }
-
-        *text_ptr = (char**)calloc(line_cnt + 1, sizeof(**text_ptr));
-
-        *text_ptr[0] = text_buffer;
-
-        int added_lines = 1;
-        for (size_t i = 0; i < true_file_size; i++) {
-            if (text_buffer[i] == '\0') {
-                (*text_ptr)[added_lines] = text_buffer + (i + 1);
-                added_lines++;
-            }
-        }
-
-        free(*text_ptr);
-    }
-
-    end_time = clock();
-
-    printf("%lf\n", (double)(end_time - start_time) / (double)iterations_cnt);
-    return 0; //text_vec.size;
+    return text_vec.size;
 }
 
 void MemoryFree(char** text, size_t len) {
